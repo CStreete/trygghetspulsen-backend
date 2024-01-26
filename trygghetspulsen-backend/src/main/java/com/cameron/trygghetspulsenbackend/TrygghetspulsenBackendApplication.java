da80658459ce3b745d6ee58329ab5acdb74c28c2
CStreete <112933415+CStreete@users.noreply.github.com>
2024-01-26 23:39:05 +0100

    Initial commit

---
+++ trygghetspulsen-backend/src/main/java/com/cameron/trygghetspulsenbackend/TrygghetspulsenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cameron.trygghetspulsenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrygghetspulsenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrygghetspulsenBackendApplication.class, args);
+	}
+
+}
---
+++ trygghetspulsen-backend/src/test/java/com/cameron/trygghetspulsenbackend/TrygghetspulsenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cameron.trygghetspulsenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrygghetspulsenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
